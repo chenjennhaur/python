92d9c1e7b64fec1cda1caee8469bf9a5c0a0005a
chenjennhaur <chenjennhaur@hotmail.com>
2017-03-30 21:46:37 +0800

    Update google_geocode.py

--- google_geocode.py
+++ google_geocode.py
@@ -2,3 +2,5 @@ import requests
 import json
 r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=Singapore+238359&key='+api)
 data = json.loads(r.text)
+data['results'][0]['geometry']['location']
+# lat or lng
