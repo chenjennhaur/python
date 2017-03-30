b9c2c43f306f9405e5f5c6d5b30fbff720b03240
chenjennhaur <chenjennhaur@hotmail.com>
2017-03-30 21:53:17 +0800

    Update google_geocode.py

--- google_geocode.py
+++ google_geocode.py
@@ -4,3 +4,6 @@ r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=Sing
 data = json.loads(r.text)
 data['results'][0]['geometry']['location']
 # lat or lng
+if (data['status'] == 'OK'):
+    for location in data['results']:
+      print(location['geometry']['location'])
